16de0ebdd80046694faac598851cdd2b05f235c8
Natarajan <nataraj.itsolution@gmail.com>
2025-01-11 11:21:08 +0530

    model signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, viewChild, viewChildren } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, effect, ElementRef, model, viewChild, viewChildren } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { RouterOutlet } from '@angular/router';
 import { ChildComponent } from './component/child/child.component';
 import { TempchildComponent } from './component/tempchild/tempchild.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, MatFormFieldModule, MatCardModule, MatInputModule,
-    ChildComponent,TempchildComponent
+    ChildComponent,TempchildComponent,FormsModule,CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -23,13 +25,24 @@ export class AppComponent implements AfterViewInit {
   // nameElement = viewChild.required<ElementRef>('name');
   // child = viewChild.required<ChildComponent>(ChildComponent);
   // childs = viewChildren(ChildComponent);
-
+  
   readonly nameElement = viewChild.required<ElementRef>('name');
   readonly child = viewChild.required(ChildComponent);
   readonly childs = viewChildren(ChildComponent);
 
-  constructor(private cref: ChangeDetectorRef) {
+  //username='Test'
+  username=model<string>('Test')
+  userobj=model<{firstname:string,lastname:string}>({
+    firstname: '',
+    lastname: ''
+  })
 
+  constructor(private cref: ChangeDetectorRef) {
+     effect(()=>{
+      if(this.username().length>5){
+        alert('Crossed more than 5 characters')
+      }
+     })
   }
 
   ngAfterViewInit(): void {
